55e9e660c351daf1e80d0e086497ff99c2a749fc
fIlM APICHAT <apichat201@hotmail.com>
2018-12-06 14:28:31 +0700

    new vb

---
+++ project VB apichat/project VB apichat/frmlab1.Designer.vb
@@ -0,0 +1,191 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.llbltotal = New System.Windows.Forms.Label()
+        Me.lbltax = New System.Windows.Forms.Label()
+        Me.lblyear = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnclose = New System.Windows.Forms.Button()
+        Me.btncal = New System.Windows.Forms.Button()
+        Me.txtsal = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(111, 19)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(217, 20)
+        Me.Label9.TabIndex = 23
+        Me.Label9.Text = "โปรเเกรมคำณวนหารายได้ทั้งปี"
+        Me.Label9.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label8.Location = New System.Drawing.Point(70, 134)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(108, 18)
+        Me.Label8.TabIndex = 22
+        Me.Label8.Text = "เงินเดิอนพนักงาน /ปี :"
+        '
+        'llbltotal
+        '
+        Me.llbltotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.llbltotal.Location = New System.Drawing.Point(181, 227)
+        Me.llbltotal.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.llbltotal.Name = "llbltotal"
+        Me.llbltotal.Size = New System.Drawing.Size(166, 16)
+        Me.llbltotal.TabIndex = 21
+        '
+        'lbltax
+        '
+        Me.lbltax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbltax.Location = New System.Drawing.Point(184, 180)
+        Me.lbltax.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbltax.Name = "lbltax"
+        Me.lbltax.Size = New System.Drawing.Size(163, 16)
+        Me.lbltax.TabIndex = 20
+        '
+        'lblyear
+        '
+        Me.lblyear.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblyear.Location = New System.Drawing.Point(184, 133)
+        Me.lblyear.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblyear.Name = "lblyear"
+        Me.lblyear.Size = New System.Drawing.Size(163, 16)
+        Me.lblyear.TabIndex = 19
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(70, 227)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 18)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "รายได้สุทธิ :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(70, 180)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 18)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "ภาษีที่ต้องชำระ :"
+        '
+        'btnclose
+        '
+        Me.btnclose.Location = New System.Drawing.Point(228, 300)
+        Me.btnclose.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnclose.Name = "btnclose"
+        Me.btnclose.Size = New System.Drawing.Size(100, 28)
+        Me.btnclose.TabIndex = 16
+        Me.btnclose.Text = "CLOSE"
+        Me.btnclose.UseVisualStyleBackColor = True
+        '
+        'btncal
+        '
+        Me.btncal.Location = New System.Drawing.Point(81, 300)
+        Me.btncal.Margin = New System.Windows.Forms.Padding(4)
+        Me.btncal.Name = "btncal"
+        Me.btncal.Size = New System.Drawing.Size(100, 28)
+        Me.btncal.TabIndex = 15
+        Me.btncal.Text = "CALCULATE"
+        Me.btncal.UseVisualStyleBackColor = True
+        '
+        'txtsal
+        '
+        Me.txtsal.Location = New System.Drawing.Point(184, 80)
+        Me.txtsal.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtsal.Name = "txtsal"
+        Me.txtsal.Size = New System.Drawing.Size(163, 20)
+        Me.txtsal.TabIndex = 13
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(70, 80)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 18)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "เงินเดิอนพนักงาน :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(416, 347)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.llbltotal)
+        Me.Controls.Add(Me.lbltax)
+        Me.Controls.Add(Me.lblyear)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnclose)
+        Me.Controls.Add(Me.btncal)
+        Me.Controls.Add(Me.txtsal)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents llbltotal As Label
+    Friend WithEvents lbltax As Label
+    Friend WithEvents lblyear As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnclose As Button
+    Friend WithEvents btncal As Button
+    Friend WithEvents txtsal As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ project VB apichat/project VB apichat/frmlab2.vb
@@ -0,0 +1,25 @@
+﻿Public Class frmlab2
+    Private Sub btnclose_Click(sender As Object, e As EventArgs) Handles btnclose.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub btncal_Click(sender As Object, e As EventArgs) Handles btncal.Click
+        Dim sel1, totalsel, comisionv As Integer
+        Dim sel2, comisionc, comisiontotal As Integer
+        sel1 = Val(txtgluta.Text)
+        sel2 = Val(txtcom.Text)
+
+        totalsel = sel1 + sel2
+        lblseltotal.Text = totalsel
+
+        comisionv = sel1 * 5 / 100
+        comisionc = sel2 * 10 / 100
+
+        lblcomgluta.Text = comisionv
+        lblcomcom.Text = comisionc
+
+        comisiontotal = comisionv + comisionc
+        lblcomtotal.Text = comisiontotal
+    End Sub
+End Class
